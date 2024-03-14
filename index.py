1297e119ca3d4a2e37bd42499950b8ec0b455b29
divpreet11 <divpreetkaur08@gmail.com>
2024-03-15 00:36:30 +0530

    matrix added

--- index.py
+++ index.py
@@ -9,15 +9,21 @@
 # print("Factorial of",num,"is",factorial(num))
 
 
-import sys
+import os
 
 def factorial(n):
     return 1 if n == 1 or n == 0 else n * factorial(n - 1)
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python script.py <number>")
-        sys.exit(1)
+    num_str = os.getenv("NUM")
+    if num_str is None:
+        print("Environment variable NUM is not set.")
+        exit(1)
     
-    num = int(sys.argv[1])
+    try:
+        num = int(num_str)
+    except ValueError:
+        print("Invalid value for NUM. Please provide an integer.")
+        exit(1)
+    
     print("Factorial of", num, "is", factorial(num))
