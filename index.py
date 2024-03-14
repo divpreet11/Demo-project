4d60763042db3f3dfa257d2ecd02835cd371522e
divpreet11 <divpreetkaur08@gmail.com>
2024-03-15 00:32:21 +0530

    matrix added

--- index.py
+++ index.py
@@ -1,9 +1,23 @@
-# Python 3 program to find factorial of given number
-def factorial(n):
+# # Python 3 program to find factorial of given number
+# def factorial(n):
 	
-	# single line to find factorial
-	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
+# 	# single line to find factorial
+# 	return 1 if (n==1 or n==0) else n * factorial(n - 1) 
 
-# Driver Code
-num = 5
-print("Factorial of",num,"is",factorial(num))
+# # Driver Code
+# num = 5
+# print("Factorial of",num,"is",factorial(num))
+
+
+import sys
+
+def factorial(n):
+    return 1 if n == 1 or n == 0 else n * factorial(n - 1)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python script.py <number>")
+        sys.exit(1)
+    
+    num = int(sys.argv[1])
+    print("Factorial of", num, "is", factorial(num))
